af82064a1f2ad63a984d29775b75fd1df5025b97
ortiztena <ortiztena@gmail.com>
2022-04-27 12:46:35 +0200

    common complete

--- 04-rest-api/back/src/app.ts
+++ 04-rest-api/back/src/app.ts
@@ -3,25 +3,23 @@ import { hotelsApi } from "pods/hotel";
 import path from "path";
 import { createRestApiServer } from "core/servers";
 import { envConstants } from "core/constants";
+import {
+  logRequestMiddleware,
+  logErrorRequestMiddleware,
+} from "common/middlewares";
 
 
 const restApiServer = createRestApiServer();
 
 
-restApiServer.use(async (req, res, next) => {
-  console.log(req.url);
-  next();
-});
+restApiServer.use(logRequestMiddleware);
 
 const staticFilesPath = path.resolve(__dirname, envConstants.STATIC_FILES_PATH);
 restApiServer.use("/", express.static(staticFilesPath));
 
 restApiServer.use("/api/hotels", hotelsApi)
 
-restApiServer.use(async (error, req, res, next) => {
-  console.error(error);
-  res.sendStatus(500)
-});
+restApiServer.use(logErrorRequestMiddleware);
 
 restApiServer.listen(envConstants.PORT, () => {
   console.log(`Server ready at port ${envConstants.PORT}`);
