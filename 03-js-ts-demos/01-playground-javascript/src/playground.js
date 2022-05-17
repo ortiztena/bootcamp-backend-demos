5b96914abec0a94db8c964cc298fa98114431766
ortiztena <ortiztena@gmail.com>
2022-05-17 16:19:58 +0200

    3,4 done

--- 03-js-ts-demos/01-playground-javascript/src/playground.js
+++ 03-js-ts-demos/01-playground-javascript/src/playground.js
@@ -1,15 +1,38 @@
-const elements = ["lorem", "ipsum", "dolor", "sit", "amet"];
-const index = 2;
-const newValue = "furor";
+const elements = [0, 1, false, 2, "", 3];
 
-const replaceAt = (arr, index, newElement) => {
-  return (newArr = [
-    ...arr.slice(0, index),
-    newElement,
-    ...arr.slice(index + 1),
-  ]);
+const compact = (arg) => {
+  if (typeof arg === "object") {
+    Object.values(obj).filter((x) => Boolean(x) === true);
+  }
+  if (typeof arg === "array") {
+    return arg.filter((x) => Boolean(x));
+  }
 };
 
-const result = replaceAt(elements, index, newValue);
-console.log(result === elements); // false
-console.log(result); // ['lorem', 'ipsum', 'furor', 'sit', 'amet'];
+//function that returns truthy values from an object
+const truthy = (obj) => {
+  const arrValues = Object.values(obj);
+  const arrKeys = Object.keys(obj);
+  const truthyValues = arrValues.filter((x) => Boolean(x) === true);
+  for (let keys in obj) {
+    let arr = [];
+    if (obj[keys] === true) {
+      arr.push(keys);
+    }
+    return arr;
+  }
+};
+
+console.log(compact(123)); // 123
+console.log(compact(null)); // null
+console.log(compact([0, 1, false, 2, "", 3])); // [1, 2, 3]
+console.log(compact({})); // {}
+console.log(
+  compact({
+    price: 0,
+    name: "cloud",
+    altitude: NaN,
+    taste: undefined,
+    isAlive: false,
+  })
+); // {name: "cloud"}
