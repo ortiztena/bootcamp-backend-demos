654558535ce9e91bc30bd6ac2e26307f4a312022
ortiztena <ortiztena@gmail.com>
2022-05-18 10:47:30 +0200

    all done

--- 03-js-ts-demos/01-playground-javascript/src/05.js
+++ 03-js-ts-demos/01-playground-javascript/src/05.js
@@ -1,7 +1,20 @@
 const elements = [0, 1, false, 2, "", 3];
 
 const compact = (arg) => {
-  return arg.filter((x) => x);
+  if (Boolean(arg) === true) {
+    if (Array.isArray(arg) === false && typeof arg === "object") {
+      return truthyObj(arg);
+    }
+    if (Array.isArray(arg) === true && arg.length > 0) {
+      return arg.filter((x) => Boolean(x));
+    }
+  }
+  return arg;
+};
+
+const truthyObj = (obj) => {
+  const truthyValues = Object.entries(obj).filter((x) => Boolean(x[1]));
+  return Object.fromEntries(truthyValues);
 };
 
 console.log(compact(123)); // 123
